816db439ae66046ebac5c7d1dc84b1663de11ae2
KeenenCrummitt <104858295+KeenenCrummitt@users.noreply.github.com>
2022-11-27 18:53:25 -0700

    creating DOM elements

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 
 // Data
 
-/*
+
 const account1 = {
   owner: 'Jonas Schmedtmann',
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
@@ -60,7 +60,28 @@ const inputTransferAmount = document.querySelector('.form__input--amount');
 const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
-*/
+
+const displayMovements = function(movements) {
+  containerMovements.innerHTML = '';
+  // .textContent = 0
+
+  movements.forEach(function (mov, i) {
+    const type = mov > 0 ? 'deposit' : 'withdrawal'
+    const html = `
+      <div class="movements__row">
+        <div class="movements__type movements__type--${type}">${
+          i + 1
+        } ${type}</div>
+        <div class="movements__value">${mov}</div>
+      </div>
+    `;
+
+        containerMovements.insertAdjacentHTML('afterbegin', html);
+
+  });
+};
+displayMovements(account1.movements);
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
