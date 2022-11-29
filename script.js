6e62ce3a2040420e9c9dc702f4453da941378bbd
KeenenCrummitt <104858295+KeenenCrummitt@users.noreply.github.com>
2022-11-29 10:59:48 -0700

    Filter method

--- script.js
+++ script.js
@@ -92,7 +92,7 @@ const createUsernames = function(accs) {
   });
 };
 createUsernames(accounts); // stw
-console.log(accounts);
+// console.log(accounts);
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
@@ -252,10 +252,18 @@ console.log(movementsDescriptions);
 
 
 
+const deposits = movements.filter(function(mov, i, arr) {
+  return mov > 0;
+});
+console.log(movements);
+console.log(deposits);
 
+const depositsFor = [];
+for (const mov of movements) if (mov > 0) depositsFor.push(mov)
+console.log(depositsFor);
 
-
-
+const withdrawals = movements.filter(mov => mov < 0);
+console.log(withdrawals);
 
 
 
