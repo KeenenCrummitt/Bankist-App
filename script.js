b1aed222260330df06b8452237eeefdb7267c53c
KeenenCrummitt <104858295+KeenenCrummitt@users.noreply.github.com>
2022-11-30 10:44:37 -0700

    Implementing Login

--- script.js
+++ script.js
@@ -88,20 +88,20 @@ const calcDisplayBalance = function(movements) {
 };
 calcDisplayBalance(account1.movements);
 
-const calcDisplaySummary = function(movements) {
-  const incomes = movements
+const calcDisplaySummary = function(acc) {
+  const incomes = acc.movements
     .filter(mov => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
   labelSumIn.textContent = `${incomes}€`;
 
-  const out = movements
+  const out = acc.movements
     .filter(mov => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
   labelSumOut.textContent = `${Math.abs(out)}€`;
 
-  const interest = movements
+  const interest = acc.movements
   .filter(mov => mov > 0)
-  .map(deposit => deposit * 1.2/100)
+  .map(deposit => deposit * acc.interestRate/100)
   .filter((int, i, arr) => {
     // console.log(arr);
     return int >= 1;
@@ -109,7 +109,7 @@ const calcDisplaySummary = function(movements) {
   .reduce((acc, int) => acc + int, 0);
   labelSumInterest.textContent = `${interest}€`;
 };
-calcDisplaySummary(account1.movements);
+
 
 const createUsernames = function(accs) {
   accs.forEach(function(acc) {
@@ -122,8 +122,38 @@ const createUsernames = function(accs) {
 };
 createUsernames(accounts); // stw
 
+//Event handler
+let currentAccount;
 
+btnLogin.addEventListener('click', function(e) {
+  // Prevent form from submitting
+  e.preventDefault();
 
+ currentAccount = accounts.find(
+  acc => acc.username === inputLoginUsername.value
+  );
+ console.log(currentAccount);
+
+ if(currentAccount?.pin === Number(inputLoginPin.value)) {
+  // Display UI and message
+  labelWelcome.textContent = `Welcome back, ${
+    currentAccount.owner.split(' ')[0]
+}`;
+containerApp.style.opacity = 100;
+
+  // Clear input fields
+  inputLoginUsername.value = inputLoginPin.value = '';
+  inputLoginPin.blur();
+
+  // Display movements
+  displayMovements(currentAccount.movements);
+
+  // Display balance
+  calcDisplayBalance(currentAccount.movements);
+  // Display summary
+  calcDisplaySummary(currentAccount);
+ }
+});
 // console.log(accounts);
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
@@ -404,6 +434,7 @@ const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
 console.log(avg1, avg2);
 */
 
+/*
 const firstWithdrawal = movements.find(mov => mov < 0);
 console.log(movements);
 console.log(firstWithdrawal);
@@ -412,7 +443,7 @@ console.log(accounts);
 
 const account = accounts.find(acc => acc.owner === 'Jessica Davis')
 console.log(account);
-
+*/
 
 
 
