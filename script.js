7456c2ef5827e6cbcdaccc19289d8d66fffacd73
KeenenCrummitt <104858295+KeenenCrummitt@users.noreply.github.com>
2022-12-04 15:59:45 -0700

    Flat map

--- script.js
+++ script.js
@@ -519,6 +519,8 @@ console.log(arr.at(-1));
 
 console.log('jonas'.at(-1));
 */
+
+/*
 console.log(movements);
 
 // EQUALITY
@@ -539,3 +541,23 @@ const deposit = mov => mov > 0;
 console.log(movements.some(deposit));
 console.log(movements.every(deposit));
 console.log(movements.filter(deposit));
+*/
+
+const arr = [[1, 2, 3], [4, 5, 6], 7, 8];
+console.log(arr.flat());
+
+const arrDeep = [[[1, 2], 3], [4, [5, 6]], 7, 8];
+console.log(arrDeep.flat(2));
+
+const overalBalance = accounts
+  .map(acc => acc.movements)
+  .flat()
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(overalBalance);
+
+// flatMap
+
+const overalBalance2 = accounts
+  .flatMap(acc => acc.movements)
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(overalBalance2);
